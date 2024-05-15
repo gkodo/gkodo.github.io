f0912417d5d550432125f98b6d71ccae4292f0b3
John D. Gómez H. pro.jdgomezh@gmail.com <pro.jdgomezh@gmail.com>
2024-05-15 04:41:08 -0500

    fix(auth): fix error on new auth method
    fix error on new auth method.

--- release.config.mjs
+++ release.config.mjs
@@ -1,35 +1,6 @@
 // noinspection JSUnusedGlobalSymbols
 // noinspection JSCheckFunctionSignatures
 
-function getCurrentDatetime() {
-	const date = new Date();
-
-	const year = date.getFullYear();
-	const month = String(date.getMonth() + 1).padStart(2, '0');
-	const day = String(date.getDate()).padStart(2, '0');
-	const hours = String(date.getHours()).padStart(2, '0');
-	const minutes = String(date.getMinutes()).padStart(2, '0');
-	const seconds = String(date.getSeconds()).padStart(2, '0');
-
-	return `${year}.${month}.${day}.${hours}.${minutes}.${seconds}`;
-}
-
-async function prepareReleaseName(options, context) {
-	if (!context.env.branch || context.env.branch.trim() === '') {
-		context.env.branch = 'RLS/Main';
-	}
-
-	if (!context.env.datetime || context.env.datetime.trim() === '') {
-		context.env.datetime = getCurrentDatetime();
-	}
-
-	context.nextRelease.name = `${context.env.branch}/${context.env.datetime}`;
-}
-
-const modifyReleaseNamePlugin = {
-	prepare: prepareReleaseName,
-};
-
 const releaseConfig = {
 	branches: ['RLS/Main'],
 	plugins: [
@@ -57,7 +28,12 @@ switch (ciEnvironment) {
 		releaseConfig.plugins.push([
 			'@semantic-release/github',
 			{
-				assets: ['dist.zip', 'CHANGELOG.md', 'LICENSE.md'],
+				releaseTitle: '${process.env.BRANCH}/${process.env.DATETIME}',
+				assets: [
+					{path: 'dist.zip', label: 'Build distributable'},
+					{path: 'CHANGELOG.md', label: 'Changelog'},
+					{path: 'LICENSE.md', label: 'License'},
+				],
 			},
 		]);
 		break;
@@ -65,14 +41,17 @@ switch (ciEnvironment) {
 		releaseConfig.plugins.push([
 			'@semantic-release/gitlab',
 			{
-				assets: ['dist.zip', 'CHANGELOG.md', 'LICENSE.md'],
+				releaseTitle: '${process.env.BRANCH}/${process.env.DATETIME}',
+				assets: [
+					{path: 'dist.zip', label: 'Build distributable'},
+					{path: 'CHANGELOG.md', label: 'Changelog'},
+					{path: 'LICENSE.md', label: 'License'},
+				],
 			},
 		]);
 		break;
 	default:
 		console.log(`Unknown CI environment: ${ciEnvironment}`);
 }
-
-releaseConfig.plugins.push(modifyReleaseNamePlugin);
 
 export default releaseConfig;
